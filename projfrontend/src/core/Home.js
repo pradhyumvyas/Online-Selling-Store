61060bb463c81633525e9693ce9af3d63d5a72bf
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-10-18 23:22:17 +0530

    Added Crousel in home page

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -2,6 +2,11 @@ import React, {useState, useEffect} from 'react'
 import {getProducts} from './helper/CoreApiCalls'
 import Base from './Base';
 import Card from './Card';
+import img1 from '../images/offer1.jpg'
+import img2 from '../images/offer2.jpg'
+import img3 from '../images/offer3.jpg'
+
+
 
 
 export default function Home() {
@@ -28,7 +33,38 @@ export default function Home() {
 
     return (
         <Base title="Home Page" description="Welcome to the Shopping Site">
+
+
+
+
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel" >
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src={img1} alt="First slide" />
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src={img2} alt="Second slide"/>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src={img3} alt="Third slide" />
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+
             <div className="cardSetting row">
+
+
                 {products.map((product, index) => {
                     return(
                         <div key={index} className="col-md-2 col-sm-12 cardSetting">
