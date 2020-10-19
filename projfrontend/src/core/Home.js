a1eaa8d0762752ef7cafa8b9a9cf623e5397cc69
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-10-20 01:14:51 +0530

    Debug heading crousel

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -37,28 +37,27 @@ export default function Home() {
 
 
 
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" >
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img class="d-block w-100" src={img1} alt="First slide" />
+              <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active" data-interval="10000">
+                    <img src={img1} class="d-block w-100" alt="..." height="170px" />
+                  </div>
+                  <div class="carousel-item" data-interval="2000">
+                    <img src={img2} class="d-block w-100" alt="..." height="170px" />
+                  </div>
+                  <div class="carousel-item">
+                    <img src={img3} class="d-block w-100" alt="..." height="170px" />
+                  </div>
                 </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src={img2} alt="Second slide"/>
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src={img3} alt="Third slide" />
-                </div>
+                <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
               </div>
-              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
-            </div>
-
 
 
 
