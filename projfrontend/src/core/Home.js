6ebecfda81bf466f25ba9e3f0cad2551ad0eb0e9
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-10-23 00:15:51 +0530

    Crousel Working Absolutely fine

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import Carousel from 'react-bootstrap/Carousel'  
 import {getProducts} from './helper/CoreApiCalls'
 import Base from './Base';
 import Card from './Card';
@@ -34,28 +35,36 @@ export default function Home() {
 
 
 
-              <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active" data-interval="10000">
-                    <img src={img1} class="d-block w-100" alt="..." height="170px" />
-                  </div>
-                  <div class="carousel-item" data-interval="2000">
-                    <img src={img2} class="d-block w-100" alt="..." height="170px" />
-                  </div>
-                  <div class="carousel-item">
-                    <img src={img3} class="d-block w-100" alt="..." height="170px" />
-                  </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
-              </div>
+<Carousel>
+  <Carousel.Item interval={1000}>
+    <img
+      className="d-block w-100"
+      src={img1}
+      alt="First slide"
+    />
+    <Carousel.Caption>
 
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+    <img
+      className="d-block w-100"
+      src={img2}
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={img3}
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
 
 
             <div className="cardSetting row">
